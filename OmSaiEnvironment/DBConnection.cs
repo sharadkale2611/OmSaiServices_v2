ad99915f9e4c12f51037003b83076073580bce84
sharadkale2611 <sharadkale2611@gmail.com>
2025-01-03 14:16:49 +0530

    after adding appsettings file to git

---
+++ OmSaiEnvironment/DBConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSaiEnvironment
+{
+	public class DBConnection
+	{
+		//public static string DefaultConnection = "Server=BOTMASTER-SRD\\MSSQLEXPRESS;Database=om_sai_services_db_v1;User Id=sa;Password=pass@1234;TrustServerCertificate=True";
+		public static string DefaultConnection = "Server=115.124.106.98;Database=om_sai_services_db_v1;User Id=omuser;Password=SaiServices#2024;TrustServerCertificate=True";
+
+	}
+}
