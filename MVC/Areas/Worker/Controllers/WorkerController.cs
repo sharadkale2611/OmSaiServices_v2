5911573cca0f131a36d5eb559a0353890a959298
LubnaPatel2245 <lubnapatel2245@gmail.com>
2025-01-09 17:35:01 +0530

    workeredit partically done

--- MVC/Areas/Worker/Controllers/WorkerController.cs
+++ MVC/Areas/Worker/Controllers/WorkerController.cs
@@ -25,6 +25,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 		private readonly WorkerDocumentService _workerDocumentService;
 		private readonly WorkerAddressService _workerAddressService;
 
+
 		public WorkerController()
 		{
 			_workerService = new WorkerService();
@@ -45,8 +46,8 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 		public IActionResult Index()
 		{
 			ViewBag.AllData = _workerService.GetAll();
-			ViewBag.Department=_departmentService.GetAll();
-			ViewBag.Sites= _siteService.GetAll();
+			ViewBag.Department = _departmentService.GetAll();
+			ViewBag.Sites = _siteService.GetAll();
 
 			ViewBag.Projects = _projectService.GetAll();
 			ViewBag.Workers = _workerService.GetAll();
@@ -56,7 +57,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 		public IActionResult Profile(int id)
 		{
 			ViewBag.AllData = _workerService.GetProfileById(id, null);
-			if(ViewBag.AllData == null)
+			if (ViewBag.AllData == null)
 			{
 				return RedirectToAction(nameof(Index));// nameof checks method compiletime to avoid errors
 			}
@@ -187,7 +188,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult Create(WorkerModel model, int ProjectId, int SiteId, int QualificationId, string MobileNumber, bool Status, string? MobileNumber2="", string? Address1="", string? Address2="")
+		public IActionResult Create(WorkerModel model, int ProjectId, int SiteId, int QualificationId, string MobileNumber, bool Status, string? MobileNumber2 = "", string? Address1 = "", string? Address2 = "")
 		{
 			try
 			{
@@ -198,7 +199,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 					TempData["success"] = "Record added successfully!";
 
 					var lastWorkerId = _workerService.Create(model);
-					
+
 					WorkerProjectSiteModel workerProjectSiteModel = new WorkerProjectSiteModel
 					{
 						WorkerId = lastWorkerId,
@@ -223,7 +224,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 					};
 					_workerMobileNumbersService.Create(workerMobileNumbersModel);
 
-					if(MobileNumber2 == "")
+					if (MobileNumber2 == "")
 					{
 						MobileNumber2 = MobileNumber;
 					}
@@ -239,7 +240,7 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 						WorkerAddressModel workerAddressModel_1 = new WorkerAddressModel
 						{
 							WorkerId = lastWorkerId,
-							AddressType	= "Permanent",
+							AddressType = "Permanent",
 							Address = Address1
 						};
 						_workerAddressService.Create(workerAddressModel_1);
@@ -296,60 +297,156 @@ namespace GeneralTemplate.Areas.Worker.Controllers
 		}
 
 
+
+
 		public IActionResult Edit(int id)
 		{
+
+			var worker = _workerService.GetById(id);
+
+			if (worker == null)
+			{
+				return NotFound();
+			}
+
+
 			ViewBag.AllData = _workerService.GetAll();
 			ViewBag.Department = _departmentService.GetAll();
-
-
-			// masters
 			ViewBag.Sites = _siteService.GetAll();
 			ViewBag.Projects = _projectService.GetAll();
-			ViewBag.Workers = _workerService.GetAll();
+			ViewBag.Worker = _workerService.GetProfileById(id);
 			ViewBag.Qualifications = _qualificationService.GetAll();
+			ViewBag.Addresses = _workerAddressService.GetByWorkerId(id);
+			ViewBag.MobilesNumbers = _workerMobileNumbersService.GetByWorkerId(id);
 
 
 
-			var p = _workerService.GetById(id);
-			return View(p);
+			return View(worker);
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult Edit(WorkerModel model)
+		public IActionResult Edit(WorkerModel model, int ProjectId, int SiteId, int QualificationId, string MobileNumber, bool Status, int WorkerMobileNumberId1, int WorkerMobileNumberId2, int WorkerAddressId1 , int WorkerAddressId2, string? MobileNumber2 = "", string? Address1 = "", string? Address2 = "")
 		{
-
 			try
 			{
+				if (!model.WorkerId.HasValue)
+				{
+					TempData["error"] = "WorkerId is required for updating.";
+					return View(model);
+				}
+
 				if (ModelState.IsValid)
 				{
 					TempData["success"] = "Record updated successfully!";
+
+					// Update main Worker details
 					_workerService.Update(model);
+
+					// Update project-site relationship
+					var workerProjectSite = _workerProjectSiteService.GetById(model.WorkerId.Value);
+					if (workerProjectSite != null)
+					{
+						workerProjectSite.SiteId = SiteId;
+						workerProjectSite.ProjectId = ProjectId;
+						workerProjectSite.Status = Status;
+						_workerProjectSiteService.Update(workerProjectSite);
+					}
+
+					// Update qualification
+					var workerQualification = _workerQualificationService.GetById(model.WorkerId.Value);
+					if (workerQualification != null)
+					{
+						workerQualification.QualificationId = QualificationId;
+						_workerQualificationService.Update(workerQualification);
+					}
+
+					// Update primary mobile number
+					WorkerMobileNumbersModel workerMobileNumbersModel = new WorkerMobileNumbersModel
+					{
+						WorkerMobileNumberId = WorkerMobileNumberId1,
+						WorkerId = model.WorkerId?? 0,
+						MobileNumber = MobileNumber
+					};
+					_workerMobileNumbersService.Create(workerMobileNumbersModel);
+
+					if (MobileNumber2 == "")
+					{
+						MobileNumber2 = MobileNumber;
+					}
+					WorkerMobileNumbersModel workerMobileNumbersModel_2 = new WorkerMobileNumbersModel
+					{
+						WorkerMobileNumberId = WorkerMobileNumberId2,
+						WorkerId = model.WorkerId ?? 0,
+						MobileNumber = MobileNumber2
+					};
+					_workerMobileNumbersService.Create(workerMobileNumbersModel_2);
+					if (Address1 != "" && Address1 != null)
+					{
+						WorkerAddressModel workerAddressModel_1 = new WorkerAddressModel
+						{
+							WorkerAddressId= WorkerAddressId1,
+							WorkerId = model.WorkerId?? 0,
+							AddressType = "Permanent",
+							Address = Address1
+						};
+						_workerAddressService.Update(workerAddressModel_1);
+
+					}
+
+					if (Address2 != "" && Address2 != null)
+					{
+						WorkerAddressModel workerAddressModel_2 = new WorkerAddressModel
+						{
+							WorkerAddressId = WorkerAddressId2,
+							WorkerId = model.WorkerId ?? 0,
+							AddressType = "Current",
+							Address = Address2
+						};
+						_workerAddressService.Update(workerAddressModel_2);
+					}
+
+					// Update Permanent address
+					//var permanentAddress = 
+					//if (permanentAddress != null && !string.IsNullOrEmpty(Address1))
+					//{
+
+					//	permanentAddress.AddressType = "Permanent";
+					//	permanentAddress.Address = Address1;
+					//	_workerAddressService.Update(permanentAddress);
+					//}
+
+					// Update Current address
+					//var currentAddress = _workerAddressService.GetById(WorkerAddressId);
+					//if (currentAddress != null && !string.IsNullOrEmpty(Address2))
+					//{
+					//	currentAddress.AddressType = "Current";
+					//	currentAddress.Address = Address2;
+					//	_workerAddressService.Update(currentAddress);
+					//}
+
+
+
+					return RedirectToAction(nameof(Index));
 				}
 				else
 				{
-					var errorMessages = new List<string>();
-					foreach (var state in ModelState)
-					{
-						foreach (var error in state.Value.Errors)
-						{
-							errorMessages.Add(error.ErrorMessage);
-						}
-					}
+					// Handle model state errors
+					var errorMessages = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
 					TempData["errors"] = errorMessages;
 				}
-
-				return RedirectToAction(nameof(Index));             // nameof checks method compiletime to avoid errors
 			}
-			catch
+			catch (Exception ex)
 			{
-				TempData["error"] = "Something went wrong!";
-				return View("Index", model);
+				TempData["error"] = $"Something went wrong! {ex.Message}";
 			}
-			//_projectService.Update(model);
-			//TempData["success"] = "Project Updated successfully!";
-			//return View(model);
+
+			return View(model);
 		}
+
+
+
+
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
--- OmSaiServices/Worker/Implementations/WorkerMobileNumbersService.cs
+++ OmSaiServices/Worker/Implementations/WorkerMobileNumbersService.cs
@@ -66,8 +66,15 @@ namespace OmSaiServices.Worker.Implementations
             return GetById(id, sp_r, mapEntity, GetParams(id));
         }
 
+		public List<WorkerMobileNumbersModel> GetByWorkerId(int id)
+		{
 
-        private List<KeyValuePair<string, object>> CreateUpdate(WorkerMobileNumbersModel model, string type)
+			// Define the mapping function
+			var mapEntity = new Func<IDataReader, WorkerMobileNumbersModel>(reader => _mapper.MapEntity<WorkerMobileNumbersModel>(reader));
+
+			return GetAll(sp_r, mapEntity, GetParams(null, id));
+		}
+		private List<KeyValuePair<string, object>> CreateUpdate(WorkerMobileNumbersModel model, string type)
         {
             if (model == null)
                 throw new ArgumentNullException(nameof(model));
@@ -96,13 +103,13 @@ namespace OmSaiServices.Worker.Implementations
         }
 
 
-        private SqlParameter[] GetParams(int? id = null, int? WorkerId = null, string? MobileNumber = null)
+        private SqlParameter[] GetParams(int? id = null, int? WorkerId = null/*, string? MobileNumber = null*/)
         {
             return new SqlParameter[]
             {
                 new SqlParameter("@WorkerMobileNumberId", id),
                 new SqlParameter("@WorkerId", WorkerId),
-                new SqlParameter("@MobileNumber", MobileNumber)
+                //new SqlParameter("@MobileNumber", MobileNumber)
 				
 			};
         }
