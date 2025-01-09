15b9b41cee59d2f344e0c1ac2457495f82d64c6e
sharadkale2611 <sharadkale2611@gmail.com>
2025-01-09 11:08:35 +0530

    Dbconnection changed

--- OmSaiEnvironment/DBConnection.cs
+++ OmSaiEnvironment/DBConnection.cs
@@ -8,9 +8,13 @@ namespace OmSaiEnvironment
 {
 	public class DBConnection
 	{
-		public static string LocalConnection = "Server=BOTMASTER-SRD\\MSSQLEXPRESS;Database=om_sai_services_db_v1;User Id=sa;Password=pass@1234;TrustServerCertificate=True";
-		public static string ProdConnection = "Server=115.124.106.98;Database=om_sai_services_db_v1;User Id=omuser;Password=SaiServices#2024;TrustServerCertificate=True";
-		public static string TestConnection = "Server=A2NWPLSK14SQL-v04.shr.prod.iad2.secureserver.net;Database=om_sai_services_db_v1;User Id = omuser; Password=YSAAS#2024;TrustServerCertificate=True";
-		public static string DefaultConnection = ProdConnection;
+		//LocalConnection
+		//public static string DefaultConnection = "Server=BOTMASTER-SRD\\MSSQLEXPRESS;Database=om_sai_services_db_v1;User Id=sa;Password=pass@1234;TrustServerCertificate=True";
+
+		//ProdConnection
+		//public static string DefaultConnection = "Server=115.124.106.98;Database=om_sai_services_db_v1;User Id=omuser;Password=SaiServices#2024;TrustServerCertificate=True";
+
+		// TestConnection
+		public static string DefaultConnection = "Server=A2NWPLSK14SQL-v04.shr.prod.iad2.secureserver.net;Database=om_sai_services_db_v1;User Id = omuser; Password=YSAAS#2024;TrustServerCertificate=True";
 	}
 }
